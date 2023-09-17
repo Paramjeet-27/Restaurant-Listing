d07a0334d4dc77e57f07364b43518a82ace5acb0
Paramjeet-27 <param.960127@gmail.com>
2023-09-17 16:57:14 +0530

    FE-Search Button Implemented on Dishes List Page

--- FrontEnd/src/DishList/DishList.jsx
+++ FrontEnd/src/DishList/DishList.jsx
@@ -7,11 +7,13 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import routes from "../routes/routes.json";
+import Input from "../components/Input/Input";
 
 const DishList = ({ indexValue }) => {
   const navigate = useNavigate();
   console.log(indexValue);
   const [dishes, setDishes] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
   const [resName, setResName] = useState("");
 
   const axios_main = axios.create({
@@ -33,6 +35,14 @@ const DishList = ({ indexValue }) => {
     navigate(routes.HOME);
   };
 
+  const searchChangehandler = (e) => {
+    setSearchValue(e.target.value.toLowerCase());
+    const searched = dishes.filter(
+      (ele) => ele.toLowerCase().indexOf(searchValue) !== -1
+    );
+    setDishes(searched);
+  };
+
   const dishesList = dishes.map((ele, index) => (
     <List key={index} sx={{ mx: 10 }}>
       <ListItem disablePadding>
@@ -46,6 +56,12 @@ const DishList = ({ indexValue }) => {
   return (
     <>
       <h1>Dishes Available at : {resName}</h1>
+      <div>
+        <Input
+          changeHandler={(e) => searchChangehandler(e)}
+          value={searchValue}
+        />
+      </div>
       {dishesList}
       <Button
         sx={{ mx: 1 }}
